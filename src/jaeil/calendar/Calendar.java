3f060c43d0c08cf8f8459919f73fee48a8e331f0
c <c@192.168.219.100>
2023-05-16 14:31:10 +0900

    Add repeat function

--- src/jaeil/calendar/Calendar.java
+++ src/jaeil/calendar/Calendar.java
@@ -21,11 +21,21 @@ public class Calendar {
 
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요");
-		int month = scanner.nextInt();
-
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMAxDaysOfMonth(month));
+		Calendar cal = new Calendar(); 
+		
+		System.out.println("반복횟수를 입력하세요");
+		int repeat = scanner.nextInt();
+		for (int i = 0; i < repeat; i++) {
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();
+			
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMAxDaysOfMonth(month));
+			
+		}
+		
+		System.out.println("시스템 종료");
+				
+		
 		scanner.close();
 
 	}
